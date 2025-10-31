df91ed379a82e42173827b325dcf37b18420a3bd
vishvajit9898 <vishvajit9898@email.com>
2025-10-31 21:47:16 +0530

    updates

--- src/router/index.js
+++ src/router/index.js
@@ -6,40 +6,49 @@ import MerchantReports from '@/pages/MerchantReports.vue'
 import MerchantBeneficiaries from '@/pages/MerchantBeneficiaries.vue'
 import Settings from '@/pages/Settings.vue'
 import Login from '@/pages/Login.vue'
-// import { useAuthStore } from '@/stores/auth'  // 🔒 temporarily disabled
-
 const routes = [
-  { path: '/', name: 'home', component: HomePage },
-  { path: '/login', name: 'login', component: Login },
-  { path: '/transactions', name: 'Transactions', component: Payouts },
-  { path: '/wallet', name: 'wallet', component: MerchantWallet },
-  { path: '/reports', name: 'reports', component: MerchantReports },
-  { path: '/beneficiary', name: 'beneficiary', component: MerchantBeneficiaries },
-  { path: '/settings', name: 'settings', component: Settings },
+  {
+    path: '/',
+    name: 'home',
+    component: HomePage,
+  },
+    {
+    path: '/login',
+    name: 'login',
+    component: Login,
+     meta: { layout: "auth" },
+  },
+    {
+    path: '/transactions',
+    name: 'Transactions',
+    component: Payouts,
+  },
+      {
+    path: '/wallet',
+    name: 'wallet',
+    component: MerchantWallet,
+  },
+        {
+    path: '/reports',
+    name: 'reports',
+    component: MerchantReports,
+  },
+
+        {
+    path: '/beneficiary',
+    name: 'beneficiary',
+    component: MerchantBeneficiaries,
+  },
+          {
+    path: '/settings',
+    name: 'settings',
+    component: Settings,
+  },
 ]
 
 const router = createRouter({
   history: createWebHistory(),
   routes,
 })
-
-// 🚫 Auth guard disabled for demo
-// router.beforeEach(async (to, from, next) => {
-//   const auth = useAuthStore()
-
-//   if (!auth.user && to.meta.requiresAuth) {
-//     await auth.getUser()
-//   }
-
-//   if (to.meta.requiresAuth && !auth.isAuthenticated) {
-//     return next('/login')
-//   }
-
-//   if (to.path === '/login' && auth.isAuthenticated) {
-//     return next('/')
-//   }
-
-//   next()
-// })
 
 export default router
