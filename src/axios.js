683c596410d9c4a31e2a50885df02b67c9b1ad3d
banna2317 <officialrathod0786@gmail.com>
2026-02-11 11:19:51 +0530

    urlupdeted

--- src/axios.js
+++ src/axios.js
@@ -2,7 +2,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://127.0.0.1:8000/api",
+  baseURL: "https://a8bd-2401-4900-88b7-ccb5-419a-b016-d5f6-8fec.ngrok-free.app/api",
   headers: {
     "Content-Type": "application/json",
     Accept: "application/json",
