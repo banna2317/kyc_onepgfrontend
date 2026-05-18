58eebd3379cc53f17a94635da347ba82a42b7fe2
banna2317 <officialrathod0786@gmail.com>
2026-05-18 17:25:41 +0530

    demo

--- src/components/ui/tabs/TabsList.vue
+++ src/components/ui/tabs/TabsList.vue
@@ -14,15 +14,22 @@ const delegatedProps = reactiveOmit(props, "class");
 </script>
 
 <template>
-  <TabsList
-    v-bind="delegatedProps"
-    :class="
-      cn(
-        'inline-flex items-center h-10 justify-center rounded-lg bg-muted p-1 text-muted-foreground',
-        props.class,
-      )
-    "
-  >
+  <TabsList v-bind="delegatedProps" :class="cn(
+    // 👇 base
+    'flex items-center h-10 rounded-lg bg-muted p-1 text-muted-foreground',
+
+    // 👇 mobile full width + scroll
+    'w-full overflow-x-auto whitespace-nowrap',
+
+    // 👇 desktop pe sirf content width
+    'md:w-fit',
+
+    // 👇 spacing
+    'gap-1 px-1',
+
+    props.class,
+  )
+    ">
     <slot />
   </TabsList>
 </template>
